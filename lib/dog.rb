32c51782c059bddc30f7045e6ced6408b371743f
Abdullah Albasha <Abdullah.a.albasha@gmail.com>
2020-07-16 21:27:59 -0400

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,18 @@
+#class definition for Dog
+class Dog
+
+end
+
+#Instantiate instances of a `class`.
+
+# define a local variable fido as an instance of a Dog
+fido = Dog.new
+fido
+
+# define a local variable snoopy as an instance of a Dog
+snoopy = Dog.new
+snoopy
+
+# define a local variable lassie as an instance of a Dog
+lassie= Dog.new
+lassie
---
+++ lib/person.rb
@@ -0,0 +1,14 @@
+#creat class definition for Person
+class Person
+
+end
+
+#Instantiate instances of a `class`.
+
+#define a local variable adele_goldberg as an instance of a Person
+adele_goldberg = Person.new
+adele_goldberg
+
+#define a local variable alan_kay as an instance of a Person
+alan_kay = Person.new
+alan_kay
